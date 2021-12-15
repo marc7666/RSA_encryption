e6b477c408dfb91518f9d9c826039ed8fd512dd2
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-12-15 22:08:03 +0100

    Implementation completed, documentation remains

--- rsa.py
+++ rsa.py
@@ -59,36 +59,70 @@ def write_p_q(p, q):
     randoms.close()
 
 
+def write_n_fi(n, fi):
+    n_fi_file = open('n_fi values.txt', 'w')
+    n_fi_file.write(str(n) + "\n")
+    n_fi_file.write(str(fi) + "\n")
+    n_fi_file.close()
+
+
+def write_e(e):
+    e_file = open('e_value.txt', 'w')
+    e_file.write(str(e))
+    e_file.close()
+
+
+def write_d(d):
+    d_file = open('d_value.txt', 'w')
+    d_file.write(str(d))
+    d_file.close()
+
+
 if __name__ == "__main__":
     print(Fore.YELLOW, "Generating 2 prime random numbers...")
-    # p, q = generate_p_q()
-    p = 5
-    q = 11
+    p, q = generate_p_q()
     write_p_q(p, q)
     time.sleep(2)  # Waiting 2 seconds
     print(Fore.GREEN, "Take a look at ", repr(str("Values_of_p_and_q.txt")), "to know the values of the variables.")
     print(Fore.RED, "The first value is ", repr(str("p")), ", and the second one is ", repr(str("q")))
-    # num_n, num_fi = generate_n_fi(p, q)
-    num_n = 55
-    num_fi = 40
-    print("n = ", num_n, " fi = ", num_fi)
-    # num_e = generate_e(num_fi)
-    num_e = 3
-    print("e = ", num_e)
-    # num_d = generate_d(num_fi, num_e)
-    num_d = 27
-    print("d = ", num_d)
+
+    num_n, num_fi = generate_n_fi(p, q)
+    write_n_fi(num_n, num_fi)
+    time.sleep(2)
+    print(Fore.GREEN, "Take a look at", repr(str("n_fi values.txt")), "to know the values of the variables.")
+    print(Fore.RED, "The first value is ", repr(str("n")), ", and the second one is ", repr(str("fi")))
+
+    num_e = generate_e(num_fi)
+    write_e(num_e)
+    time.sleep(2)
+    print(Fore.GREEN, "Take a look at", repr(str("e_value.txt")), "to know the value of the variable.")
+
+    num_d = generate_d(num_fi, num_e)
+    write_d(num_d)
+    time.sleep(2)
+    print(Fore.GREEN, "Take a look at", repr(str("d_value.txt")), "to know the value of the variable.")
+
+    # Writing keys in correspondent files
+    print(Fore.YELLOW, "Writing the keys in his correspondent file...")
+    time.sleep(2)
     write_keys_in_files(num_n, num_e, num_d)
     pub_k = open('Public_key.txt', 'r')
     public_k = int(pub_k.readline()), int(pub_k.readline())
     pub_k.close()
-    print("Public key: ", public_k)
+    print("The public key is: ", public_k)
     pri_k = open('Private_key.txt', 'r')
     priv_k = int(pri_k.readline()), int(pri_k.readline())
     pri_k.close()
-    print("Private key: ", priv_k)
+    print("The private key is: ", priv_k)
+
+    # Encryption and decryption
     message_orig = 12
     num_crypto = encrypt(message_orig, num_e, num_n)
+    print(Fore.MAGENTA, "Encrypting...")
+    time.sleep(2)
     print("Cryptogram = ", num_crypto)
+
     orig_m = decrypt(num_crypto, num_d, num_n)
+    print(Fore.MAGENTA, "Decrypting...")
+    time.sleep(2)
     print("Original message = ", orig_m)
